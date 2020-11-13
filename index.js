29d1dce3ad0896f4cb52a4dac3d6818d26c229d0
Van lam <vanlam020499@gmail.com>
2020-11-13 14:01:10 +0700

    changed index.js

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     return res.send('API Movie!');
 });
 
+app.get('/bye', (req, res) => {
+    return res.send('Good bye!');
+});
+
 app.listen(port, () => {
     console.log(`Server started at: 127.0.0.1:${port}`)
 });
